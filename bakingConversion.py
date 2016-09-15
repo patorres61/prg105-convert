f63b6e45f1be0d7259b0db3d13367a7e6b48ee14
Phyllis Torres <ptorres@students.mchenry.edu>
2016-09-14 21:12:25 -0500

    Add files via upload

--- bakingConversion.py
+++ bakingConversion.py
@@ -4,37 +4,38 @@
 
 # the function must be defined before it may be used
 # tsp will behave as a local variable
-tsp = 0
-TBL = 0
+teaspoon = 0
+tablespoon = 0
 cup = 0
 runAgain = ' '
 
+
 def convert(mils):
-    tsp = round((float(mils) * 0.2), 2)
-    return tsp
+    teaspoon = round((float(mils) * 0.2), 2)
+    return teaspoon
 
 
 print("This program will convert milliliters to teaspoons, tablespoons, and cups.\n")
 
-while runAgain != 'q':                   #entering q will quit the program
+while runAgain != 'q':  # entering q will quit the program
     # accept user input
     milliliters = raw_input("How many milliliters do you wish to convert? \n")
 
     # call the function
-    tsp = convert(milliliters)
+    teaspoon = convert(milliliters)
 
-    print (milliliters + " Milliliters is equal to " + str(tsp) + " teaspoons.\n")
-    if tsp >= 3:
-        if tsp < 48:
-            TBL = tsp / 3
-            print (str(tsp) + " teaspoons = " + str(TBL) + " tablespoons.")
+    print (milliliters + " Milliliters is equal to " + str(teaspoon) + " teaspoons.\n")
+    if teaspoon >= 3:
+        if teaspoon < 48:
+            tablespoon = teaspoon / 3
+            print (str(teaspoon) + " teaspoons = " + str(tablespoon) + " tablespoons.")
         else:
-            cup = tsp / 48
-            print (str(tsp) + " teaspoons = " + str(cup) + " cups.")
+            cup = teaspoon / 48
+            print (str(teaspoon) + " teaspoons = " + str(cup) + " cups.")
 
-    print("\nWould you like to try again? ")  #ask user if they would like to try again
-    print("Enter 'q' for 'quit' or press enter to continue: ") #explain how to quit or to continue
-    runAgain = raw_input() #accept input to determine if user wants to go again
+    print("\nWould you like to try again? ")  # ask user if they would like to try again
+    print("Enter 'q' for 'quit' or press enter to continue: ")  # explain how to quit or to continue
+    runAgain = raw_input()  # accept input to determine if user wants to go again
 
-#program wrap-up
+# program wrap-up
 print("\nThanks for running my Baking Conversion program! Goodbye!")
